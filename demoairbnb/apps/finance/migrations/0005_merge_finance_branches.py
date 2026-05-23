c846d410bd15334957eb25442db262d7c3d95313
Salcedo0 <juansalcedo.yepes@gmail.com>
2026-05-22 23:30:56 -0500

    cambuios

--- demoairbnb/apps/finance/migrations/0005_merge_finance_branches.py
+++ demoairbnb/apps/finance/migrations/0005_merge_finance_branches.py
@@ -3,7 +3,8 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('finance', '0001_initial'),
+        ('finance', '0002_initial'),
+        ('finance', '0004_voucher_propertyprofitabilityreport'),
     ]
 
     operations = []
